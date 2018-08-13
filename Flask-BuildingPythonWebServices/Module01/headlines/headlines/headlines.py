e315ecaf989cc8feb20be4f91b09ceb8e0ffb7b2
Tim Ricks <ricks.tc@gmail.com>
2018-08-13 17:12:15 -0400

    Chapter 4: User Input for Our Headlines Project - Finished working through chapter 4.

--- Flask-BuildingPythonWebServices/Module01/headlines/headlines/headlines.py
+++ Flask-BuildingPythonWebServices/Module01/headlines/headlines/headlines.py
@@ -1,5 +1,12 @@
 import feedparser
-from flask import Flask, render_template
+import os
+import json
+from tvars import OWM_API_KEY, OER_APP_ID
+from urllib.parse import quote
+from urllib.request import urlopen
+from flask import Flask
+from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
@@ -8,16 +15,86 @@ RSS_FEEDS = {
     'cnn': 'http://rss.cnn.com/rss/edition.rss',
     'fox': 'http://feeds.foxnews.com/foxnews/latest',
     'iol': 'http://www.iol.co.za/cmlink/1.640'
-    }
+}
+
+DEFAULTS = {
+    'publication': 'bbc',
+    'city': 'Chicago',
+    'currency_from': 'GBP',
+    'currency_to': 'USD'
+}
+
+WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid={}"
+CURRENCY_URL = "https://openexchangerates.org/api/latest.json?app_id={}"
 
 @app.route("/")
-@app.route("/<publication>")
-def get_news(publication="bbc"):
-    feed = feedparser.parse(RSS_FEEDS[publication])
+def home():
+    """ Get customized headlines, based on user input or default """
+    publication = request.args.get('publication')
+    if not publication:
+        publication = DEFAULTS['publication']
+    articles = get_news(publication)
+        
+    city = request.args.get('city')
+
+    if not city:
+        city = DEFAULTS['city']
+    weather = get_weather(city)
+
+    # Get customized currency based on the user input or default
+    currency_from = request.args.get("currency_from")
+    if not currency_from:
+        currency_from = DEFAULTS['currency_from']
+
+    currency_to = request.args.get('currency_to')
+    if not currency_to:
+        currency_to = DEFAULTS['currency_to']
+
+    rate, currencies = get_rate(currency_from, currency_to)
+
+
     return render_template(
         "home.html",
-        articles=feed['entries']
+        articles=articles,
+        weather=weather,
+        currency_from = currency_from,
+        currency_to = currency_to,
+        rate = rate, currencies=sorted(currencies),
     )
+
+def get_news(query):
+    """ Gets the news """
+    if not query or query.lower() not in RSS_FEEDS:
+        publication = DEFAULTS['publication']
+    else:
+        publication = query.lower()
+
+    feed = feedparser.parse(RSS_FEEDS[publication])
+    return feed['entries']
+
+def get_weather(query):
+    """ Gets the weather from the open weather map api """
+    query = quote(query)
+    url = WEATHER_URL.format(query, OWM_API_KEY)
+    data = urlopen(url).read()
+    parsed = json.loads(data)
+    weather = None
+    if parsed.get('weather'):
+        weather = {
+            "description": parsed['weather'][0]['description'],
+            "temperature": parsed['main']['temp'],
+            "city": parsed['name'],
+            "country": parsed['sys']['country']
+        }
+    return weather
+
+def get_rate(frm, to):
+    """ Calculates currency conversion (poorly, because it just uses the USD instead of direct) """
+    all_currencies = urlopen(CURRENCY_URL.format(OER_APP_ID)).read()
+    parsed = json.loads(all_currencies).get('rates')
+    frm_rate = parsed.get(frm.upper())
+    to_rate = parsed.get(to.upper())
+    return (to_rate/frm_rate, parsed.keys())
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
