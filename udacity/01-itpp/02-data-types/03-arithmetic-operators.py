1ccbf686cd6ffda3157b6fea9869212c56a07a50
Timothy Ricks <timothy.c.ricks@wvsp.gov>
2019-07-25 09:16:32 -0400

    Incremental

--- udacity/01-itpp/02-data-types/03-arithmetic-operators.py
+++ udacity/01-itpp/02-data-types/03-arithmetic-operators.py
@@ -1,3 +1,14 @@
 # Write an expression that calculates the average of 23, 32 and 64
 # Place the expression in this print statement
 print((23 + 32 + 64)/3)
+
+
+# Fill this in with an expression that calculates how many tiles are needed.
+tiles = (9*7)+(5*7)
+tiles_per_package = 6
+print(tiles)
+
+# Fill this in with an expression that calculates how many tiles will be left over.
+print(17*tiles_per_package - tiles)
+
+
